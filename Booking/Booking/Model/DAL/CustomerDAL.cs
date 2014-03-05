bc3a3867d0e7e4dbd663f771907658b4447538cf
Marike Grinde <mg222cd@student.lnu.se>
2014-03-05 17:24:00 +0100

    mer kod

--- Booking/Booking/Model/DAL/CustomerDAL.cs
+++ Booking/Booking/Model/DAL/CustomerDAL.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Configuration;
@@ -11,6 +13,35 @@ namespace Booking.Model.DAL
         public IEnumerable<Customer> GetCustomers()
         {
             string connectionString = WebConfigurationManager.ConnectionStrings["BookingConnectionString"].ConnectionString;
+
+            //skapa anslutningsobjekt
+            using (var conn = new SqlConnection(connectionString))
+            {
+                //ungefärlig storlek på listobjektet
+                var customers = new List<Customer>(100);
+
+                //kommando för att exec lagrad procedur
+                var cmd = new SqlCommand("usp_GetCustomers", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //öppna anslutningen (vill man göra så sent som möjligt)
+                conn.Open();
+
+                using(var reader = cmd.ExecuteReader())
+                {
+                    //läs post för post
+                    while (reader.Read())
+                    {
+                        
+                    }
+
+
+                }
+
+                customers.TrimExcess();
+
+                return customers;
+            }
         }
     }
 }
