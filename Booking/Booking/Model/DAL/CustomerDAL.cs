b1eec0050ca36f6318465665fcdec2efe3d6d64c
Marike Grinde <mg222cd@student.lnu.se>
2014-03-05 17:37:52 +0100

    mer kod

--- Booking/Booking/Model/DAL/CustomerDAL.cs
+++ Booking/Booking/Model/DAL/CustomerDAL.cs
@@ -30,16 +30,27 @@ namespace Booking.Model.DAL
                 using(var reader = cmd.ExecuteReader())
                 {
                     var customerIdIndex = reader.GetOrdinal("CustomerId"); //returnerar heltal (med index tror jag)
-                    var nameIndex = reader.GetOrdinal("Name");
+                    var nameIndex = reader.GetOrdinal("Name"); //efterfrågas kolumn med namn som ej existerar kastas ett undantag
                     var addressIndex = reader.GetOrdinal("address");
                     var postalIndex = reader.GetOrdinal("postal");
+                    var cityIndex = reader.GetOrdinal("city");
                     var phoneIndex = reader.GetOrdinal("Phone");
                     var emailIndex = reader.GetOrdinal("Email");
 
                     //läs post för post, så länge Read returnerar True
                     while (reader.Read())
                     {
-                        
+                        //samlingsobjekt av typen List
+                        customers.Add(new Customer 
+                        {
+                            CustomerId = reader.GetInt32(customerIdIndex),
+                            Name = reader.GetString(nameIndex),
+                            Address = reader.GetString(addressIndex),
+                            Postal = reader.GetInt32(postalIndex),
+                            City = reader.GetString(cityIndex),
+                            Phone = reader.GetString(phoneIndex),
+                            Email = reader.GetString(phoneIndex)
+                        });
                     }
 
 
