00a7b302af957fa3014ea187653fbbd9e4d498f9
Marike Grinde <mg222cd@student.lnu.se>
2014-03-05 17:48:16 +0100

    mer kod

--- Booking/Booking/Model/Service.cs
+++ Booking/Booking/Model/Service.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Booking.Model.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,13 @@ namespace Booking.Model
     //Serviceklass som "pratar med " dataåtkomstklassen
     public class Service
     {
+        private CustomerDAL _customerDAL;
+
+        private CustomerDAL CustomerDAL
+        {
+            get { return _customerDAL ?? (_customerDAL = new CustomerDAL()); }
+        }
+
         public IEnumerable<Customer> GetCustomers()
         {
 
