e608449ad144625a7312f629d19e75dfdb076045
Marike Grinde <mg222cd@student.lnu.se>
2014-03-06 23:12:23 +0100

    mer kod i CustomerDAL

--- Booking/Booking/Model/DAL/CustomerDAL.cs
+++ Booking/Booking/Model/DAL/CustomerDAL.cs
@@ -41,10 +41,6 @@ namespace Booking.Model.DAL
 
         #region CRUD-metoder
 
-        /// <summary>
-        /// Hämtar alla kunder ur Customer
-        /// </summary>
-        /// <returns>samling med ref till Customer-objektet</returns>
         public IEnumerable<Customer> GetCustomers()
         {
             //skapa och initiera anslutningsobjekt
@@ -58,6 +54,9 @@ namespace Booking.Model.DAL
                     //kommando för att exec lagrad procedur
                     var cmd = new SqlCommand("[appSchema].[usp_GetCustomers]", conn);
                     cmd.CommandType = CommandType.StoredProcedure;
+
+                    //öppna anslutning till databasen
+                    conn.Open();
 
                     using (var reader = cmd.ExecuteReader())
                     {
@@ -91,35 +90,143 @@ namespace Booking.Model.DAL
                     return customers;
 
                 }
-                catch (Exception)
+                catch
                 {
                     throw new ApplicationException("Fel uppstod i samband med hämtning av kunder från databasen");
                 }
             }
         }
 
-        //Hitta
         public Customer GetCustomerById(int customerId)
         {
-            throw new NotImplementedException();
+            //skapa och initiera anslutningsobjekt
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    //kommando för att exec lagrad procedur
+                    var cmd = new SqlCommand("[appSchema].[usp_GetCustomerById]", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //skickar med kundId till lagrade proceduren
+                    cmd.Parameters.Add("@CustomerId", SqlDbType.Int, 4).Value = customerId;
+
+                    //öppna anslutning till databasen
+                    conn.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        var customerIdIndex = reader.GetOrdinal("CustomerId"); //returnerar heltal (med index tror jag)
+                        var nameIndex = reader.GetOrdinal("Name"); //efterfrågas kolumn med namn som ej existerar kastas ett undantag
+                        var addressIndex = reader.GetOrdinal("address");
+                        var postalIndex = reader.GetOrdinal("postal");
+                        var cityIndex = reader.GetOrdinal("city");
+                        var phoneIndex = reader.GetOrdinal("Phone");
+                        var emailIndex = reader.GetOrdinal("Email");
+
+                        if (reader.Read())
+                        {
+                            return new Customer
+                            {
+                                CustomerId = reader.GetInt32(customerIdIndex), //översätts till ett C#-objekt av typen Customer
+                                Name = reader.GetString(nameIndex),
+                                Address = reader.GetString(addressIndex),
+                                Postal = reader.GetInt32(postalIndex),
+                                City = reader.GetString(cityIndex),
+                                Phone = reader.GetString(phoneIndex),
+                                Email = reader.GetString(phoneIndex)
+                            };
+                        }
+                        return null;
+                    }
+                }
+                catch
+                {
+                    throw new ApplicationException("Fel uppstod i samband med hämtning av kunder från databasen");
+                }
+            }
         }
 
-        //Infoga
         public void InsertCustomerById(Customer customer)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+                    //skapar och initierar Sql-objekt som kör lagrade proceduren
+                    SqlCommand cmd = new SqlCommand("[appSchema].[usp_InsertCustomer]", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    //lägger till parametrar
+                    cmd.Parameters.Add("@Name", SqlDbType.VarChar, 40).Value = customer.Name;
+                    cmd.Parameters.Add("@Address", SqlDbType.VarChar, 40).Value = customer.Address;
+                    cmd.Parameters.Add("@Postal", SqlDbType.Int, 4).Value = customer.Postal;
+                    cmd.Parameters.Add("@City", SqlDbType.VarChar, 25).Value = customer.City;
+                    cmd.Parameters.Add("@Phone", SqlDbType.VarChar, 20).Value = customer.Phone;
+                    cmd.Parameters.Add("@Email", SqlDbType.VarChar, 50).Value = customer.Email;
+
+                    //parameter som tar emot värde från lagrade proceduren (i detta fall med nya postens customerId)
+                    cmd.Parameters.Add("@CustomerId", SqlDbType.Int, 4).Direction = ParameterDirection.Output;
+
+                    //öppnar anslutning till databasen
+                    conn.Open();
+
+                    //metod för att exec lagrade proceduren
+                    customer.CustomerId = (int)cmd.Parameters["@CustomerId"].Value;
+
+                }
+                catch
+                {
+                    throw new ApplicationException("Fel uppstod då kund skulle infogas.")
+                }
+            }
         }
 
-        //Uppdatera
         public void UpdateCustomer(Customer customer)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+                    //lagrad procedur
+                    SqlCommand cmd = new SqlCommand("[appSchema].[usp_UpdateCustomer]", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    //lägger till parametrar
+                    cmd.Parameters.Add("@Name", SqlDbType.VarChar, 40).Value = customer.Name;
+                    cmd.Parameters.Add("@Address", SqlDbType.VarChar, 40).Value = customer.Address;
+                    cmd.Parameters.Add("@Postal", SqlDbType.Int, 4).Value = customer.Postal;
+                    cmd.Parameters.Add("@City", SqlDbType.VarChar, 25).Value = customer.City;
+                    cmd.Parameters.Add("@Phone", SqlDbType.VarChar, 20).Value = customer.Phone;
+                    cmd.Parameters.Add("@Email", SqlDbType.VarChar, 50).Value = customer.Email;
+
+                    //öppnar anslutning
+                    conn.Open();
+
+                    //exekverar den lagrade proceduren
+                    cmd.ExecuteNonQuery();
+                }
+                catch
+                {
+                    throw new ApplicationException("Fel uppstod när kund skulle uppdateras");
+                }
+                
+            }
         }
 
-        //Radera
         public void DeleteCustomer(int customerId)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+
+                }
+                catch (Exception)
+                {
+                    
+                    throw;
+                }
+            }
         }
 
 
