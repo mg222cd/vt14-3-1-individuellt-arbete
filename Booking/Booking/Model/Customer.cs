2662abbea4850c09d94d82e1a7ab3c4c2e3a3baf
Marike Grinde <mg222cd@student.lnu.se>
2014-03-07 15:02:17 +0100

    lagt till validering i Affärslogiklagret

--- Booking/Booking/Model/Customer.cs
+++ Booking/Booking/Model/Customer.cs
@@ -11,21 +11,32 @@ namespace Booking.Model
         //Egeneskaper som motsvarar kolumner i tabellen.
         public int CustomerId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage="Ett namn måste anges.")]
+        [StringLength(40)]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Adress måste anges.")]
+        [StringLength(40)]
         public string Address { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Postnr måste anges.")]
+        [StringLength(6)]
+        [RegularExpression("([0-9]+)")]
         public int Postal { get; set; }
 
+        [Required(ErrorMessage = "Ort måste anges.")]
+        [StringLength(25)]
         [Required]
         public string City { get; set; }
 
+        [Required(ErrorMessage = "Telefonnummer måste anges.")]
+        [StringLength(20)]
         [Required]
         public string Phone { get; set; }
 
+        [Required(ErrorMessage = "Email måste anges.")]
+        [StringLength(50)]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "E-postadressen är inte giltig.")]
         [Required]
         public string Email { get; set; }
         
