cf26b7c13cf4b596cb0848ca6162c1c5d503382b
Marike Grinde <mg222cd@student.lnu.se>
2014-03-12 15:48:22 +0100

    skapat BookingDAL

---
+++ Booking/Booking/Model/DAL/BookingDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Booking.Model.DAL
+{
+    public class BookingDAL
+    {
+        #region Fält
+
+        #endregion
+
+        #region Konstruktorer
+
+        #endregion
+
+        #region Privata hjälpmetoder
+
+        #endregion
+
+        #region CRUD-metoder
+
+        #endregion
+    }
+}
